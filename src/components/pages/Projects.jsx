e3c70bda99c43a5f26801c03cee412847f18d4ff
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-08-13 15:27:54 -0300

    Componente de delete
    Adição da função de excluir projetos para o componente de delete.

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -12,6 +12,7 @@ import styles from './Projects.module.css';
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false);
+    const [projectMessage, setProjectMessage] = useState('');
 
     const location = useLocation();
     let message = '';
@@ -36,6 +37,21 @@ function Projects() {
         }, 500);
     }, []);
 
+    function removeProject(id) {
+        fetch(`http://localhost:3000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res => res.json)
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id));
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err));
+    }
+
     return (
         <div className={styles['project_container']}>
             <div className={styles['title_container']}>
@@ -43,6 +59,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => <ProjectCard
@@ -51,6 +68,7 @@ function Projects() {
                         budget={project.budget}
                         category={project.category.name}
                         key={project.id}
+                        handleRemove={removeProject}
                     />)
                 }
                 {!removeLoading && <Loading />}
